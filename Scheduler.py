4ef6fea7fb32461055f1f0d3950b8bda3452122b
FlpWgda <flpwgda@gmail.com>
2021-04-27 01:47:23 +0200

    scheduler

---
+++ Scheduler.py
@@ -0,0 +1,15 @@
+
+a = [[] for i in range(12)]
+print(a)
+
+counter = 0
+counter += 1
+counter += 1
+print(counter)
+
+if 2.0 == 2:
+    print(2)
+
+lista = ['ab','bc','cd']
+if 'bc' in lista:
+    print('Yes')
